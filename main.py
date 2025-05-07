3a2af4943f50e760fa30fe9dd546c8e972cfa820
Norwood91 <robnorwood91@gmail.com>
2025-05-07 16:49:49 -0700

    Created some of the necessary variables and also created a function for user to choose level.

--- main.py
+++ main.py
@@ -1,16 +1,37 @@
 # IMPORT THE RANDOM MODULE
-
+from random import randint
 # WELCOME THE USER AND DISPLAY INSTRUCTIONS
+print("Welcome to the Number Guessing Game!")
+print("In this game, you have to try to guess the number I'm thinking of from 1-100.")
 
 # CREATE A VARIABLE TO STORE THE RANDOM NUMBER FOR THE USER TO GUESS
+number_to_guess = randint(1,100)
+print(number_to_guess)
 
-# CREATE A VARIABLE TO STORE THE USER'S GUESS
+# CREATE CONSTANTS TO STORE THE NUMBER OF ATTEMPTS THE USER STARTS WITH, DEPENDENT ON THE DIFFICULTY
+EASY_MODE_ATTEMPTS = 10
+HARD_MODE_ATTEMPTS = 5
 
-# CREATE A VARIABLE TO STORE THE NUMBER OF ATTEMPTS THE USER STARTS WITH, DEPENDENT ON THE DIFFICULTY
+# CREATE A VARIABLE TO STORE THE GAME'S LEVEL
+game_level = input("Choose your difficulty level. Type 'easy' or 'hard': ").lower()
+
 
 # CREATE A FUNCTION FOR USER TO CHOOSE A DIFFICULTY LEVEL
+def choose_difficulty(choice):
+    if choice == "q":
+        return
+    elif choice == "":
+        print("That isn't a valid choice. Enter either 'easy' or 'hard' to continue.")
+    elif choice == "easy":
+        print(f"You chose easy mode. You have {EASY_MODE_ATTEMPTS} attempts to guess the number, good luck!")
+        return EASY_MODE_ATTEMPTS
+    elif choice == "hard":
+        print(f"You chose hard mode. You have {HARD_MODE_ATTEMPTS} attempts to guess the number, good luck!")
+        return HARD_MODE_ATTEMPTS
+    
+# CREATE A VARIABLE TO STORE THE NUMBER OF GUESSES THE USER HAS LEFT   
+num_of_guesses_left = choose_difficulty(game_level)
 
-# CREATE A FUNCTION TO CHECK THE USERS GUESS AGAINST THE ANSWER
 
-# CREATE A WHILE LOOP TO KEEP THE GAME GOING UNTIL THE USER GUESSES THE CORRECT ANSWER
+
 
