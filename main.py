107defbc74a12d05213477f79c80e73574c75d10
Norwood91 <robnorwood91@gmail.com>
2025-05-09 01:06:08 -0700

    Added a func to check user guess against answer. Created game while loop. Project finished. Going to write a test for the check_answer func.

--- main.py
+++ main.py
@@ -7,6 +7,7 @@ EASY_MODE_ATTEMPTS = 10
 HARD_MODE_ATTEMPTS = 5
 INVALID_CHOICE_MSG = "That isn't a valid choice. Enter either 'easy' or 'hard' to continue."
 
+
 number_to_guess = randint(1,100)
 print(number_to_guess)
 
@@ -14,6 +15,9 @@ difficulty_level_or_quit = input("Choose your difficulty level. Type 'easy' or '
 
 
 def choose_difficulty(choice):
+    """Returns either the number of attempts the user has
+    or it quits the game or returns an invalid choice message"""
+
     if choice == "easy":
         print(f"You chose easy mode. You have {EASY_MODE_ATTEMPTS} attempts to guess the number, good luck!")
         return EASY_MODE_ATTEMPTS
@@ -31,15 +35,42 @@ def choose_difficulty(choice):
         return INVALID_CHOICE_MSG
     
     
-# CREATE A VARIABLE TO STORE THE NUMBER OF GUESSES THE USER HAS LEFT   
 num_of_guesses_left = choose_difficulty(difficulty_level_or_quit)
 
-# CREATE A FUNCTION FOR USER TO CHOOSE A DIFFICULTY LEVEL
 
+def check_answer(user_guess, answer):
+    """Checks the user's guess and returns updated guesses and result status"""
     
+    global num_of_guesses_left
+    game_over = "GAME OVER."
 
+    if user_guess > answer:
+        num_of_guesses_left -= 1
+        print(f"Your answer, {user_guess}, was too high, try again.")
+        print(f"You have {num_of_guesses_left} guesses left.")
+        if num_of_guesses_left == 0:
+            print(game_over)
+    elif user_guess < answer:
+        num_of_guesses_left -= 1
+        print(f"Your answer, {user_guess}, was too low, try again.")
+        print(f"You have {num_of_guesses_left} guesses left.")
+        if num_of_guesses_left == 0:
+            print(game_over)
+    else:
+        print(f"Your answer, {user_guess}, was CORRECT! YOU WIN!")
+        return True
 
-
+while num_of_guesses_left > 0:
+    user_guess = int(input("What's the number I'm thinking of?: "))
+    compared_guess_to_answer = check_answer(user_guess, number_to_guess)
+    
+    if compared_guess_to_answer != True:
+        continue
+    else:
+        break
+ 
+        
+  
 
 
 
