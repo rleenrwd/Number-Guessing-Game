2b2220762a8d9ce2acf70adcadd2b59bd0abc023
Norwood91 <robnorwood91@gmail.com>
2025-05-09 01:49:15 -0700

    Created a test file for the check_answer func. Successful test for guessing too high.

---
+++ test_check_answer_function.py
@@ -0,0 +1,29 @@
+import unittest
+import main
+from unittest.mock import patch
+from io import StringIO
+from main import user_guess
+
+class CheckAnswerTest(unittest.TestCase):
+    """ Class to test the check_answer function in main.py"""
+
+    main.num_of_guesses_left = 3
+
+    # This decorator replaces sys.stdout with a StringIO object, so that the print statements can be 
+    # captured and examined in the tests
+    @patch('sys.stdout', new_callable=StringIO)
+    def test_guess_too_high(self, mock_stdout):
+        result = main.check_answer(user_guess, 50)
+        # This will check that result is 'None' since there's no return statement only print statements
+        self.assertIsNone(result)
+        
+        # This will store the output, which should be "Your answer, {user_guess}, was too high, try again." 
+        # And also, "You have {num_of_guesses_left} guesses left."
+        output = mock_stdout.getvalue()
+        
+        self.assertEqual(f"Your answer, {user_guess}, was too high, try again.\nYou have {main.num_of_guesses_left} guesses left.\n", output)
+
+        self.assertEqual(main.num_of_guesses_left, 2)
+
+if __name__ == '__main__':
+    unittest.main()
